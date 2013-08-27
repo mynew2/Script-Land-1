2b5b9a47c3e3820d8827c549703d7dd381117f22
zdroid9770 <zdroid9770@yahoo.com>
2013-08-27 16:30:43 -0500

    Renaming

--- Blizzlike/ArcPro-ArcEmu/Lua/achievements/giddyup!.lua
+++ Blizzlike/ArcPro-ArcEmu/Lua/achievements/giddyup!.lua
@@ -8,4 +8,15 @@ Author: ArcPro Speculation
 ~~End of License... Please Stand By...
 -- ArcPro Speculation, January 19, 2011 - 2013. ]]
 
-print("Failed")
+function OnCast(event, pPlayer, SpellId, pSpellObject)
+	if(SpellId == 33388) then
+		pPlayer:AddAchievement(891)
+	else
+		if(pPlayer:HasAchievement(891)) then
+			pPlayer:RemoveAchievement(891)
+		else
+		end
+	end
+end
+
+RegisterServerHook(4, "OnCast")
