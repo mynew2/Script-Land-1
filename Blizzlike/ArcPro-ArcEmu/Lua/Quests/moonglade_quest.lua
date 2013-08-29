44bd26f8397ca93c5a3bfee0ed338d56007b758c
zdroid9770 <zdroid9770@yahoo.com>
2013-08-28 22:00:40 -0500

    Renamed and moved quests to their own directory

--- Blizzlike/ArcPro-ArcEmu/Lua/Quests/moonglade_quest.lua
+++ Blizzlike/ArcPro-ArcEmu/Lua/Quests/moonglade_quest.lua
@@ -1,12 +1,12 @@
---[[ WoTD License - 
+--[[ ArcPro Speculation License - 
 This software is provided as free and open source by the
-team of The WoTD Team. This script was written and is
+team of The ArcPro Speculation Team. This script was written and is
 protected by the GPL v2. Please give credit where credit
 is due, if modifying, redistributing and/or using this 
 software. Thank you.
-Thank: zdroid9770; for the Script
+Author: ArcPro Speculation
 ~~End of License... Please Stand By...
--- WoTD Team, Janurary 19, 2010. ]]
+-- ArcPro Speculation, January 19, 2011 - 2013. ]]
 
 function Moonglade_OnAccept(pUnit, Event)
 	pUnit:CastSpellOnTarget(19027, pUnit:GetClosestPlayer())
