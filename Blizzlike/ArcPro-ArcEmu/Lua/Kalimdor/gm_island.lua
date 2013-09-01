6c65587a2edf738685462432c328d32c18209398
zdroid9770 <zdroid9770@yahoo.com>
2013-08-31 21:44:02 -0500

    GM Island renamed and moved

--- Blizzlike/ArcPro-ArcEmu/Lua/Kalimdor/gm_island.lua
+++ Blizzlike/ArcPro-ArcEmu/Lua/Kalimdor/gm_island.lua
@@ -1,12 +1,12 @@
---[[ WoTD License - 
+--[[ ArcPro Speculation License - 
 This software is provided as free and open source by the
-team of The WoTD Team. This script was written and is
+team of The ArcPro Speculation Team. This script was written and is
 protected by the GPL v2. Please give credit where credit
 is due, if modifying, redistributing and/or using this 
 software. Thank you.
-Thank: Grimton1337; for the Script
+Author: Grimton1337, ArcPro Speculation
 ~~End of License... Please Stand By...
--- WoTD Team, Janurary 19, 2011 - 2013. ]]
+-- ArcPro Speculation, January 19, 2011 - 2013. ]]
 
 function GM_Island_Player_In(event, pPlayer)
 local ZoneId = pPlayer:GetZoneId()
@@ -15,7 +15,7 @@ local ZoneId = pPlayer:GetZoneId()
 			return 0;
 		elseif(pPlayer:IsGm() == false) then
 			local Name = pPlayer:GetName()
-			pPlayer:SendAreaTriggerMessage(Name.." is not a Game Master/Admin to enter this area. Your character will be frozen until a Game Master/Admin appears.")
+			pPlayer:SendAreaTriggerMessage(Name.." is not a Game Master/Administrator to enter this area. Your character will be frozen until a Game Master/Admin appears.")
 			pPlayer:CastSpell(9454)
 		end
 	end
