44519c476de4a5fd002cc718d2316bdf5ba62e89
zdroid9770 <zdroid9770@yahoo.com>
2013-09-23 01:30:35 -0500

    Error list updated: Stormwind City - Trade Center + Entrance... sooo much yet a small area

--- Projects/Deathwing's Revenge/Error.lua
+++ Projects/Deathwing's Revenge/Error.lua
@@ -1,5 +1,59 @@
+NAME - ID - DESCRIPTION
 -----NPC-----
-
+Thomas Miller - 3518 - Missing waypoints
+	GUID:{30985}
+Stormwind City Patroller - 1976 - No waypoints for many
+	GUIDS:{30990, 31091, 31082}
+Stormwind City Guard - 68 - Missing spawn X: -8984.813477 Y: 485.533875 Z: 96.795860 O: 2.249219
+	Missing waypoints for GUID:{31129}
+Darkmoon Faire Mystic Mage - 54334 - Not suppose to be here until Darkmoon Faire
+	GUID:{31036}
+Anastasia - 44773 - Missing waypoints
+	GUID:{30999}
+Genevieve - 44774 - Missing gossip text
+	GUID:{30998}
+Ol`Emma - 3520 - Missing waypoints
+	GUID:{30992}
+Officer Jaxon - 14423 - Missing waypoints
+	GUID:{31103}
+Dwarven Construction Worker - 28596 - Missing emote to mine (but actually rebuilding Stormwind from Deathwing)
+	GUID:{30969, 31084, 31088, 31080, 31081, 30968, 30964, 30967}
+Contruction Worker - 28569 - Missing emote to mine (but actually rebuilding Stormwind from Deathwing)
+	GUID:{30971, 31086, 31089, 31085, 31087, 30966, 30970, 30965}
+Stormwind Gryphon Rider - 51348 - Missing Waypoints
+	GUID:{31094, 31093}
+Aldwin Laughlin - 4974 - No option to petitioner(create guild) nor change tabard (designer) for guild
+	GUID:{31078}
+Rebecca Laughlin - 5193 - No option to change tabard (designer) for guild and no vendor
+	GUID:{31075}
+Shay Pressler - 46602 - When you open the vendor, the item tooltip for "Recipe: Broiled Dragon Feast" will come up and never go away (even if you just exit out) until you go to the next page
+	GUID:{31077}
+-----MOB-----
+Stormwind Rat - 49540 - No waypoints/random waypoints
+GUID:{30978, 30979, }
+-----ITEM-----
+Recipe: Broiled Dragon Feast - 62799 - See NPC: "Shay Pressler"
 -----QUEST-----
-
+Guardians of Hyjal: Call of the Ancients - 29390, 29391 - Unselectable quest for the alliance "Hero`s Call Board"
 -----OBJECT-----
+Come Visit the Darkmoon Faire! - 209116/209124 - Not suppose to be here until Darkmoon Faire
+	Entry: 209116 GUID:{46517, 46502, 46497, 46399}
+	Entry: 209124 GUID:{46470, 46504, 46398}
+Darkmoon Faire Banner - 179965 - Not suppose to be here until Darkmoon Faire
+	GUID:{45946}
+Balloon Bunch - 209114 - Not suppose to be here until Darkmoon Faire
+	GUID:{46503, 46114}
+Hero`s Call Board - 206111 - See quest: "Guardians of Hyjal: Call of the Ancients"
+	GUID:{45945}
+The Severed Head of Nefarian - 179882 - Needs to spawn when a raid group gets first kill on realm then stays forever until realm resets
+	GUID:{46410}
+-----ACHIEVEMENTS-----
+Profit Sharing - 5201 - Not counting criteria even when 1 player collects cold, nothing happens. Nor when 5 players have collected. Stays at 00G 00S 00C
+	(G = Gold; S = Silver; C = Copper)
+-----CORE-----
+Left click a NPC will do a right click to open the gossip menu
+
+When you talk to a vendor; All bags will open. (Only the backpack needs to open)
+
+Depositing 88G 00S 00C in guild vault will send "Internal guild error"
+	(G = Gold; S = Silver; C = Copper)
